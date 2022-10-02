44b60fb55b36b29733e7395299741f2627161df8
Karabas890 <113842344+Karabas890@users.noreply.github.com>
2022-10-02 19:06:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) {
 
         Battle b = new Battle();
-        Pokemon p1 = new Palkia("Уолтер Уайт", 100);
-        Pokemon p2 = new Blitzle("Джесси Пинкмен", 100);
-        Pokemon p3 = new Zebstrika("Густаво Фринг", 100);
-        Pokemon p4 = new NidoranM("Сол Гудман", 100);
-        Pokemon p5 = new Nidorino("Хэнк Шредер", 100);
-        Pokemon p6 = new Nidoking("Майк Эрмантраут", 100);
+        Pokemon p1 = new Palkia("Уолтер Уайт", 1);
+        Pokemon p2 = new Blitzle("Джесси Пинкмен", 1);
+        Pokemon p3 = new Zebstrika("Густаво Фринг", 1);
+        Pokemon p4 = new NidoranM("Сол Гудман", 1);
+        Pokemon p5 = new Nidorino("Хэнк Шредер", 1);
+        Pokemon p6 = new Nidoking("Майк Эрмантраут", 1);
         b.addAlly(p1);
         b.addAlly(p2);
         b.addAlly(p4);
